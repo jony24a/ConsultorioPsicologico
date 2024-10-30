dc89ccc6fc7e9f5aad994eb13574073beab4d5f2
jony24a <127144327+jony24a@users.noreply.github.com>
2024-10-29 22:41:03 -0500

    Instalacion de axios

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,31 +1,23 @@
-// src/components/Calendar.jsx
 import { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link
+import { Link } from 'react-router-dom'; 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './calendarStyles.css'; // Importa tu archivo CSS personalizado
+import './calendarStyles.css'; 
 
-// Configuración del localizador de fechas
+
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = () => {
   const [events, setEvents] = useState([]);
 
-  // Simulación de obtención de eventos desde un backend
+  
   useEffect(() => {
     // Aquí podrías hacer una llamada a tu API para obtener eventos
     const fetchedEvents = [
       {
-        title: 'Cita con el psicólogo',
-        start: new Date(2024, 10, 25, 10, 0), // Año, mes, día, hora, minuto
-        end: new Date(2024, 10, 25, 11, 0),
-      },
-      {
-        title: 'Reunión de equipo',
-        start: new Date(2024, 10, 26, 14, 0),
-        end: new Date(2024, 10, 26, 15, 0),
-      },
+        
+      }
     ];
     setEvents(fetchedEvents);
   }, []);
@@ -39,13 +31,13 @@ const MyCalendar = () => {
           events={events}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 600 }} // Aumentar la altura para mejor visualización
+          style={{ height: 600 }} 
           className="rounded-lg"
           formats={{
             dayFormat: (date) => moment(date).format('dddd'),
-            monthHeaderFormat: (date) => moment(date).format('MMMM YYYY'), // Formato de mes
-            dayHeaderFormat: (date) => moment(date).format('MMMM DD, YYYY'), // Formato de encabezado de día
-            agendaDateFormat: (date) => moment(date).format('MMMM DD, YYYY'), // Formato de fecha en la agenda
+            monthHeaderFormat: (date) => moment(date).format('MMMM YYYY'), 
+            dayHeaderFormat: (date) => moment(date).format('MMMM DD, YYYY'), 
+            agendaDateFormat: (date) => moment(date).format('MMMM DD, YYYY'), 
           }}
         />
       </div>
