e8cb41ce6100ad0d6f9cd01f73b9c76e9b70738b
jony24a <127144327+jony24a@users.noreply.github.com>
2024-11-16 22:36:29 -0500

    Modifique un pequeno campo del api.ts

--- consultorio-psicologico/src/services/api.ts
+++ consultorio-psicologico/src/services/api.ts
@@ -15,9 +15,17 @@ const api = axios.create({
 export const createPaciente = (paciente: Paciente) => api.post('/pacientes', paciente);
 export const getPacientes = () => api.get<Paciente[]>('/pacientes');
 
+// Nuevo Endpoint para actualizar Paciente
+export const updatePaciente = (numeroDocumento: string, paciente: Paciente) => 
+  api.put(`/pacientes/${numeroDocumento}`, paciente);
+
 // Endpoints para Personal
 export const getPersonal = () => api.get<Personal[]>('/personal');
 export const createPersonal = (personal: Personal) => api.post<Personal>('/personal', personal);
+
+// Nuevo Endpoint para actualizar Personal
+export const updatePersonal = (numeroDocumento: string, personal: Personal) => 
+  api.put(`/personal/${numeroDocumento}`, personal);
 
 // Endpoints para Citas
 export const getCitas = () => api.get<Cita[]>('/citas');
