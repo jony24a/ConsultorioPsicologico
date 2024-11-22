82e31d9e84b59cb6d9bd268ada5ef9c86788ec25
Jony24a <jeijei24a@gmail.com>
2024-11-22 17:11:05 -0500

    Solo se le cambia el tipo number a text

--- consultorio-psicologico/src/components/AgregarPaciente.vue
+++ consultorio-psicologico/src/components/AgregarPaciente.vue
@@ -46,7 +46,7 @@
             <input
               v-model="paciente.telefono"
               id="telefono"
-              type="number"
+              type="text"
               required
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5"
             />
